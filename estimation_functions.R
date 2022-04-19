90090cd2c48f21ee43489fd59b9b9f7b196c5fcd
Qinglong Tian <46699477+tianqinglong@users.noreply.github.com>
2022-04-19 01:43:41 -0500

    add gh quadature

--- estimation_functions.R
+++ estimation_functions.R
@@ -1,5 +1,21 @@
+library(fastGHQuad)
 # This function computes $E_s{\rho^{1 or 2}(y)|x}$ for the internal x, coef_y_x_s and sigma_y must be estimated using external data
-E_S_RHO_X <- function(beta_rho, rho_pwr, x_mat_no_intercept, coef_y_x_s, sigma_y_x_s, num_of_replications = 500) {
+E_S_RHO_X <- function(beta_rho, rho_pwr, x_mat_no_intercept, coef_y_x_s, sigma_y_x_s,
+                      num_of_replications = 18) {
+  coef_y_x_s <- matrix(coef_y_x_s, ncol = 1)
+  beta_rho <- matrix(beta_rho, ncol = 1)
+  
+  x_mat <- cbind(1, x_mat_no_intercept)
+  mean_y_x_s <- c(x_mat %*% coef_y_x_s)
+  
+  # A <- 1/2/sigma_y_x_s^2-rho_pwr*beta_rho[2]
+  # if (A > 0) {
+  #   B <- rho_pwr*beta_rho[1]+mean_y_x_s/c(sigma_y_x_s)^2
+  # 
+  #   out <- exp(B^2/4/c(A)-mean_y_x_s^2/2/c(sigma_y_x_s))/sqrt(2*c(A*sigma_y_x_s^2))
+  #   return(out)
+  # }
+  # 
   coef_y_x_s <- matrix(coef_y_x_s, ncol = 1)
   beta_rho <- matrix(beta_rho, ncol = 1)
   
@@ -8,27 +24,35 @@ E_S_RHO_X <- function(beta_rho, rho_pwr, x_mat_no_intercept, coef_y_x_s, sigma_y
   
   num_of_x <- nrow(x_mat_no_intercept)
   e_s_rho_x <- numeric(num_of_x)
-  error_terms <- rnorm(num_of_replications, 0, sigma_y_x_s)
+  
+  ghList <- gaussHermiteData(num_of_replications)
+  xList <- ghList$x
+  wList <- ghList$w
   
   for (i in 1:num_of_x) {
-    y_plus_error <- mean_y_x_s[i]+error_terms
+    y_plus_error <- sqrt(2)*c(sigma_y_x_s)*xList+c(mean_y_x_s[i])
     
     yMat <- cbind(y_plus_error, y_plus_error^2)
     rho_values <- c(yMat %*% beta_rho)
     
-    e_s_rho_x[i] <- mean(exp(rho_pwr*rho_values))
+    e_s_rho_x[i] <- sum(wList*exp(rho_pwr*rho_values))/sqrt(pi)
   }
   
   return(e_s_rho_x)
 }
 
 # This function computes $E_s(\rho(y))$, here y_s_vector is external
-E_S_RHO <- function(beta_rho, y_s_external) {
-  y_s_vector <- matrix(y_s_external, ncol = 1)
+E_S_RHO <- function(beta_rho, Mu_Y_S, Sig_Y_S, num_of_repl = 18) {
+  ghList <- gaussHermiteData(num_of_repl)
+  xList <- ghList$x
+  wList <- ghList$w
+  
+  beta_rho <- matrix(beta_rho, ncol = 1)
+  y_s_vector <- sqrt(2)*Sig_Y_S*xList+Mu_Y_S
   y_mat <- cbind(y_s_vector, y_s_vector^2)
   
   rho_values <- c(y_mat %*% beta_rho)
-  e_s_rho <- mean(exp(rho_values))
+  e_s_rho <- sum(wList*exp(rho_values))/sqrt(pi)
   
   return(e_s_rho)
 }
@@ -44,44 +68,51 @@ COMPUTE_TAU <- function(e_s_rho_x, e_s_rho2_x, c_ps, piVal) {
 
 # This function computes $E_t(\tau)$, x_t_mat_external.
 # Here e_s_rho_x and e_s_rho2_x are related to x_t_mat_external, not the target sample
-E_T_TAU <- function(x_t_mat_external, e_s_rho_x, e_s_rho2_x, c_ps, piVal) {
-  tau_vec <- COMPUTE_TAU(x_t_mat_external, e_s_rho_x, e_s_rho2_x, c_ps, piVal)
+E_T_TAU <- function(e_s_rho_x, e_s_rho2_x, c_ps, piVal) {
+  tau_vec <- COMPUTE_TAU(e_s_rho_x, e_s_rho2_x, c_ps, piVal)
   out <- mean(tau_vec)
-  
   return(out)
 }
 
 # This function computes $E_t(d\log\Rho/d\beta)$, y_s_external is external
-E_T_D_LOG_RHO_DIV_D_BETA <- function(beta_rho, y_s_external, c_ps) {
+E_T_D_LOG_RHO_DIV_D_BETA <- function(beta_rho, Mu_Y_S, Sig_Y_S, c_ps, num_of_rep = 18) {
   beta_rho <- matrix(beta_rho, ncol = 1)
-  y_s_external <- matrix(y_s_external, ncol = 1)
+  
+  ghList <- gaussHermiteData(num_of_rep)
+  xList <- ghList$x
+  wList <- ghList$w
+  y_s_external <- sqrt(2)*Sig_Y_S*xList+Mu_Y_S
+    
   yMat <- cbind(y_s_external, y_s_external^2)
   rhoVecSrc <- c(exp(yMat %*% beta_rho))
   # \beta_1: that is y
-  outBeta1 <- mean(rhoVecSrc*y_s_external)/c_ps
+  outBeta1 <- sum(wList*rhoVecSrc*y_s_external)/c_ps/sqrt(pi)
   # \beta_2: that is y^2
-  outBeta2 <- mean(rhoVecSrc*(y_s_external^2))/c_ps
+  outBeta2 <- sum(wList*rhoVecSrc*(y_s_external^2))/c_ps/sqrt(pi)
   
   return(c(outBeta1, outBeta2))
 }
 
 # This function computes $\E_t(d\log\Rho/d\beta|x)$, x might be internal/external
-E_T_D_LOG_RHO_DIV_D_BETA_X <- function(beta_rho, x_mat_no_intercept, coef_y_x_s, sigma_y_x_s, e_s_rho_x) {
+E_T_D_LOG_RHO_DIV_D_BETA_X <- function(beta_rho, x_mat_no_intercept, coef_y_x_s, sigma_y_x_s, e_s_rho_x, num_of_rep = 18) {
   x_mat <- cbind(1, x_mat_no_intercept)
   y_x_mean <- c(x_mat %*% coef_y_x_s)
   beta_rho <- matrix(beta_rho, ncol = 1)
   
   num_of_x <- nrow(x_mat)
-  error_terms <- rnorm(num_of_x, 0, sigma_y_x_s)
   out_mat <- matrix(nrow = num_of_x, ncol = 2)
-  
+
+  ghList <- gaussHermiteData(num_of_rep)
+  xList <- ghList$x
+  wList <- ghList$w
+    
   for (i in 1:num_of_x) {
-    y_plus_error <- y_x_mean[i]+error_terms
+    y_plus_error <- sqrt(2)*c(sigma_y_x_s)*xList+c(y_x_mean[i])
     y_tmp_mat <- cbind(y_plus_error, y_plus_error^2)
     rhoVecSrc <- exp(y_tmp_mat %*% beta_rho)
     
-    tmp1 <- mean(rhoVecSrc*y_plus_error)
-    tmp2 <- mean(rhoVecSrc*y_plus_error^2)
+    tmp1 <- sum(wList*rhoVecSrc*y_plus_error)/sqrt(pi)
+    tmp2 <- sum(wList*rhoVecSrc*y_plus_error^2)/sqrt(pi)
     
     out_mat[i,] <- c(tmp1, tmp2)
   }
@@ -92,16 +123,17 @@ E_T_D_LOG_RHO_DIV_D_BETA_X <- function(beta_rho, x_mat_no_intercept, coef_y_x_s,
 }
 
 # This function computes S(\x), for both internal/external x
-Compute_S <- function(beta_rho, x_mat_no_intercept, coef_y_x_s, sigma_y_x_s, e_s_rho_x, y_s_external, c_ps) {
+Compute_S <- function(beta_rho, x_mat_no_intercept, coef_y_x_s, sigma_y_x_s, e_s_rho_x, Mu_Y_S, Sig_Y_S, c_ps) {
   e_t_d_log_rho_div_d_beta_x <- E_T_D_LOG_RHO_DIV_D_BETA_X(beta_rho, x_mat_no_intercept, coef_y_x_s, sigma_y_x_s, e_s_rho_x)
-  e_t_d_log_rho_div_d_beta <- E_T_D_LOG_RHO_DIV_D_BETA(beta_rho, y_s_external, c_ps)
+  e_t_d_log_rho_div_d_beta <- E_T_D_LOG_RHO_DIV_D_BETA(beta_rho, Mu_Y_S, Sig_Y_S, c_ps)
   e_t_d_log_rho_div_d_beta <- matrix(e_t_d_log_rho_div_d_beta, ncol = 2, nrow = nrow(x_mat_no_intercept), byrow = T)
   
   return(e_t_d_log_rho_div_d_beta_x-e_t_d_log_rho_div_d_beta)
 }
 
 # This function computes the efficient score function for the internal data
-ComputeEfficientScore <- function(beta_rho, sData, tData, piVal, sData_ext, tDat_ext, coef_y_x_s, sigma_y_x_s) {
+ComputeEfficientScore <- function(beta_rho, sData, tData, piVal, tDat_ext, coef_y_x_s, sigma_y_x_s,
+                                  Mu_Y_S, Sig_Y_S) {
   num_of_source <- nrow(sData)
   num_of_target <- nrow(tData)
   num_of_total <- num_of_source+num_of_target
@@ -113,8 +145,7 @@ ComputeEfficientScore <- function(beta_rho, sData, tData, piVal, sData_ext, tDat
   y_internal_mat <- cbind(y_internal, y_internal^2)
   rho_internal <- c(exp(y_internal_mat %*% beta_rho))
   
-  y_s_external <- sData_ext[,"Y"]
-  c_ps <- E_S_RHO(beta_rho, y_s_external)
+  c_ps <- E_S_RHO(beta_rho, Mu_Y_S, Sig_Y_S)
   
   xMatAll <- rbind(sData[,-1], tData)
   
@@ -135,12 +166,12 @@ ComputeEfficientScore <- function(beta_rho, sData, tData, piVal, sData_ext, tDat
   
   # Find the second part of b_1
   ## First find S(x), where x is internal
-  s_x_internal <- Compute_S(beta_rho, xMatAll, coef_y_x_s, sigma_y_x_s, e_s_rho_x, y_s_external, c_ps)
+  s_x_internal <- Compute_S(beta_rho, xMatAll, coef_y_x_s, sigma_y_x_s, e_s_rho_x, Mu_Y_S, Sig_Y_S, c_ps)
   ## For E_t(\tau\S) and E_t(\tau) (using tDat_ext)
   e_s_rho_x_ext <- E_S_RHO_X(beta_rho, 1, tDat_ext, coef_y_x_s, sigma_y_x_s)
   e_s_rho2_x_ext <- E_S_RHO_X(beta_rho, 2, tDat_ext, coef_y_x_s, sigma_y_x_s)
   tau_x_external <- COMPUTE_TAU(e_s_rho_x_ext, e_s_rho2_x_ext, c_ps, piVal)
-  s_x_external <- Compute_S(beta_rho, tDat_ext, coef_y_x_s, sigma_y_x_s, e_s_rho_x_ext, y_s_external, c_ps)
+  s_x_external <- Compute_S(beta_rho, tDat_ext, coef_y_x_s, sigma_y_x_s, e_s_rho_x_ext, Mu_Y_S, Sig_Y_S, c_ps)
   
   e_t_tau <- mean(tau_x_external)
   e_t_tau_s <- colMeans(matrix(tau_x_external, ncol = 2, nrow = length(tau_x_external), byrow = F)*s_x_external)
@@ -156,13 +187,13 @@ ComputeEfficientScore <- function(beta_rho, sData, tData, piVal, sData_ext, tDat
 }
 
 # This function computes the variance of beta hat
-ComputeCovarianceMatrix <- function(beta_rho, sData, tData, piVal, sData_ext, tDat_ext, coef_y_x_s, sigma_y_x_s) {
+ComputeCovarianceMatrix <- function(beta_rho, sData, tData, piVal, tDat_ext, coef_y_x_s, sigma_y_x_s,
+                                    Mu_Y_S, Sig_Y_S) {
   num_of_target <- nrow(tData)
   num_of_source <- nrow(sData)
   
   multiplier <- 1/(1-piVal)
-  y_s_external <- sData_ext[,"Y"]
-  c_ps <- E_S_RHO(beta_rho, y_s_external)
+  c_ps <- E_S_RHO(beta_rho, Mu_Y_S, Sig_Y_S)
   
   e_s_rho2_x <- E_S_RHO_X(beta_rho, 2, tData, coef_y_x_s, sigma_y_x_s)
   e_s_rho_x <- E_S_RHO_X(beta_rho, 1, tData, coef_y_x_s, sigma_y_x_s)
@@ -170,12 +201,12 @@ ComputeCovarianceMatrix <- function(beta_rho, sData, tData, piVal, sData_ext, tD
   tau_x_t <- COMPUTE_TAU(e_s_rho_x, e_s_rho2_x, c_ps, piVal)
   tau_x_t <- matrix(c(tau_x_t), ncol = 2, nrow = num_of_target, byrow = F)
   
-  s_x_t <- Compute_S(beta_rho, tData, coef_y_x_s, sigma_y_x_s, e_s_rho_x, y_s_external, c_ps)
+  s_x_t <- Compute_S(beta_rho, tData, coef_y_x_s, sigma_y_x_s, e_s_rho_x, Mu_Y_S, Sig_Y_S, c_ps)
   
   e_s_rho_x_ext <- E_S_RHO_X(beta_rho, 1, tDat_ext, coef_y_x_s, sigma_y_x_s)
   e_s_rho2_x_ext <- E_S_RHO_X(beta_rho, 2, tDat_ext, coef_y_x_s, sigma_y_x_s)
   tau_x_external <- COMPUTE_TAU(e_s_rho_x_ext, e_s_rho2_x_ext, c_ps, piVal)
-  s_x_external <- Compute_S(beta_rho, tDat_ext, coef_y_x_s, sigma_y_x_s, e_s_rho_x_ext, y_s_external, c_ps)
+  s_x_external <- Compute_S(beta_rho, tDat_ext, coef_y_x_s, sigma_y_x_s, e_s_rho_x_ext, Mu_Y_S, Sig_Y_S, c_ps)
   
   e_t_tau <- mean(tau_x_external)
   e_t_tau_s <- colMeans(matrix(tau_x_external, ncol = 2, nrow = length(tau_x_external), byrow = F)*s_x_external)
